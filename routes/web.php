c647f84e36d0b0e33ccaceec9f1e79b33917d344
mredirisinghe <mredirisinghee@gmail.com>
2024-10-28 12:02:53 +0000

    Update SEU/IS/20/ICT/058 edit routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/token', function () { return csrf_token(); });
+Route::post('employee', [EmployeeController::class, 'store']); 
+Route::get('employee/all', [EmployeeController::class, 'all']);
+Route::get('employee/{id}/edit', [EmployeeController::class, 'edit']); 
+Route::post('employee/{id}', [EmployeeController::class, 'update']); 
+Route::delete('employee/{id}', [EmployeeController::class, 'delete']);
