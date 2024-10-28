7ddafcb33bb671eadfde68eeb22c586a2f2ac5c8
mredirisinghe <mredirisinghee@gmail.com>
2024-10-28 12:05:05 +0000

    Update SEU/IS/20/ICT/048 edit reads function

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class TimeController extends Controller
 {
-    //
+    public function all() { return TimeController::all(); } 
+    
+    public function get($id) { return TimeController::findOrFail($id); }
 }
