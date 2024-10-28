1878f3024e8e42c34e9214c37e48bfe1340fca30
mredirisinghe <mredirisinghee@gmail.com>
2024-10-28 12:10:26 +0000

    Update SEU/IS/20/ICT/021 edit Update function

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class TimeController extends Controller
 {
-    //
+    public function update(Request $request, $id) { 
+        $employee = TimeController::findOrFail($id);
+        return $employee->update($request->all()); }
 }
